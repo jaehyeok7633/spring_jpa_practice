edfca7633c8138ce6555f3cfde61d1c7d54b5667
jaehyeok <jaehyeok7633@naver.com>
2024-05-16 16:04:14 +0900

    종합 연습 예제 (게시물 수정, 삭제) 처리, swagger ui(API 명세서) 사용법

--- src/main/java/com/study/jpa/chap05_practice/entity/HashTag.java
+++ src/main/java/com/study/jpa/chap05_practice/entity/HashTag.java
@@ -3,7 +3,6 @@ package com.study.jpa.chap05_practice.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-
 @Setter @Getter
 @ToString(exclude = {"post"})
 @EqualsAndHashCode(of = "id")
@@ -13,15 +12,31 @@ import lombok.*;
 @Entity
 @Table(name = "tbl_hash_tag")
 public class HashTag {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "tag_no")
     private Long id;
-    
+
     private String tagName; // 해시태그 이름
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
     @JoinColumn(name = "post_no")
     private Post post;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
